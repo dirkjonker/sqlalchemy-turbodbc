f249862797dc2b580d5ac384a39d782d0a42cbbd
Dirk Jonker <dirkjonker@gmail.com>
2020-05-27 09:51:31 +0200

    Release version 0.1.1 as stable
    No issues were reported over the past 3 years that required
    changing any code and with 263 downloads in the past 30 days
    I will consider this package to be stable now.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # sqlalchemy-turbodbc
-SQLAlchemy connector/dialect for Turbodbc.
+SQLAlchemy connector/dialect for connecting to MS SQL Server using Turbodbc.
 
-Currently only MS SQL Server is supported, I currently have no plans
-to support other databases, but feel free to fork this repository or create
-pull requests.
+Currently only MS SQL Server is supported, there are no plans
+to support other databases, but I welcome any pull requests that add support for more databases.
 
-This software is in Alpha state, and not tested very well. Please report bugs
-using the issue tracker in Github.
+Please report bugs using the issue tracker in Github.
+
+If you want to take ownership of this repository and package, please open an issue!
 
 
 ## Installation
@@ -16,7 +16,7 @@ to install the dialect.
 
 
 ## Usage
-This should work exactly like the `mssql+pyodbc` dialect as described in the [SQLAlchemy documentation here](https://docs.sqlalchemy.org/en/13/dialects/mssql.html#module-sqlalchemy.dialects.mssql.pyodbc).
+This works exactly like the `mssql+pyodbc` dialect as described in the [SQLAlchemy documentation here](https://docs.sqlalchemy.org/en/13/dialects/mssql.html#module-sqlalchemy.dialects.mssql.pyodbc).
 To create a connection using this simply use the `mssql+turbodbc` protocol.
 
 For example:
@@ -25,5 +25,4 @@ For example:
 engine = create_engine('mssql+turbodbc://scott:tiger@mydsn')
 ```
 
-For more information please see the SQLAlchemy documentation
-https://docs.sqlalchemy.org/en/13/dialects/mssql.html#module-sqlalchemy.dialects.mssql.pyodbc
+For more information please see the [SQLAlchemy documentation](https://docs.sqlalchemy.org/en/13/dialects/mssql.html#module-sqlalchemy.dialects.mssql.pyodbc).
--- setup.py
+++ setup.py
@@ -8,21 +8,39 @@ if sys.version_info < (3, 3):
     test_requirements.append('mock')
 
 
+long_description = """
+# sqlalchemy-turbodbc
+
+SQLAlchemy connector/dialect for connecting to MS SQL Server using Turbodbc.
+
+This works exactly like the `mssql+pyodbc` dialect as described in the [SQLAlchemy documentation here](https://docs.sqlalchemy.org/en/13/dialects/mssql.html#module-sqlalchemy.dialects.mssql.pyodbc).
+To create a connection using this simply use the `mssql+turbodbc` protocol.
+
+For example:
+
+```python
+engine = create_engine('mssql+turbodbc://scott:tiger@mydsn')
+```
+
+For more information please see the [SQLAlchemy documentation](https://docs.sqlalchemy.org/en/13/dialects/mssql.html#module-sqlalchemy.dialects.mssql.pyodbc).
+"""
+
+
 setup(
     name='sqlalchemy_turbodbc',
-    version='0.1.0',
+    version='0.1.1',
     description='SQLAlchemy dialect for Turbodbc',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Dirk Jonker',
     author_email='dirkjonker@gmail.com',
     url='https://github.com/dirkjonker/sqlalchemy-turbodbc',
-    download_url='https://github.com/dirkjonker/sqlalchemy-turboodbc/tarball/0.1.0',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Database',
     ],
     keywords='sql server sqlalchemy turbodbc mssql',
     packages=find_packages(),
